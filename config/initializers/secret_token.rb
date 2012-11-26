48d95139efcf89543c286ea002e2a298c17509ca
D. Falkner <dfalkner@surgeworks.com>
2012-11-25 18:35:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LothCouchdb::Application.config.secret_token = 'cb0a094421b299dbc8504463d0edc3db8af3ad2f4dbf12c7f1bba6b1563d1490775c40645c9d1f0c6c0ce7edab31263be4e9d87a42f6323818642b7c6fcaecb5'
